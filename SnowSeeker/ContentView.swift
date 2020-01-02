44d23c417dec7436195788647f084bd653b615bb
Brent Michalski <Brent.Michalski@gmail.com>
2020-01-02 09:06:15 -0600

    Initial Commit

---
+++ SnowSeeker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SnowSeeker
+//
+//  Created by Michalski (US), James B on 1/2/20.
+//  Copyright © 2020 Perlguy, Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
